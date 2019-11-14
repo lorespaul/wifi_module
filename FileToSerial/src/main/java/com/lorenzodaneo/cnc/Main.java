69e765a00a3440aa17d1fd7324214cb5cc22a2a9
Lorenzo <lorenzo.daneo91@gmail.com>
2019-11-14 23:33:38 +0100

    test method

--- FileToSerial/src/main/java/com/lorenzodaneo/cnc/Main.java
+++ FileToSerial/src/main/java/com/lorenzodaneo/cnc/Main.java
@@ -2,6 +2,7 @@ package com.lorenzodaneo.cnc;
 
 
 import org.apache.commons.lang3.StringUtils;
+import java.io.IOException;
 
 /**
  * @author Lorenzo Daneo (mail to lorenzo.daneo@coolshop.it)
@@ -19,47 +20,94 @@ public class Main {
 
         if(serial.connect("/dev/ttyS33")){
 
-            GCodeFileReader gCode = new GCodeFileReader("cnc.ngc");
-            String command;
-            int executed = 0;
-            while ((command = gCode.getCommand()) != null){
-
-                if(command.startsWith("G") || command.startsWith("M") || command.startsWith("F")) {
-                    serial.writeLine(command);
-
-                    String line;
-                    int breacker = 0;
-                    do {
-                        line = serial.readLine();
-                        System.out.println(line);
-                        if(breacker > 1000){
-                            System.out.println("Breacker goes up to 1000.");
-                            break;
-                        }
-                        breacker++;
-                    } while (!line.startsWith("CommandTime="));
-
-                    line = line.replace("CommandTime=", "");
-                    if(StringUtils.isNumeric(line)){
-                        int wait = Integer.valueOf(line) + 10;
-                        Thread.sleep(wait);
-                    } else {
-                        System.out.println("Error in command timing reading.");
-                        serial.writeLine("G01 X0 Y0 Z0");
-                        break;
-                    }
-                    executed++;
-                }
-
-            }
+            int executed = executeFromFile(serial);
+//            int executed = executeTest(serial);
 
             System.out.println("Executed " + executed + " commands!");
 
-            gCode.close();
+
             serial.close();
 
         }
 
+    }
+
+
+    private static int executeFromFile(TwoWaySerialCommunication serial) throws IOException, InterruptedException {
+
+        GCodeFileReader gCode = new GCodeFileReader("cnc.ngc");
+        String command;
+        int executed = 0;
+        while ((command = gCode.getCommand()) != null){
+
+            if(command.startsWith("G") || command.startsWith("M") || command.startsWith("F")) {
+                serial.writeLine(command);
+
+                String line;
+                int breaker = 0;
+                do {
+                    line = serial.readLine();
+                    System.out.println(line);
+                    if(breaker > 1000){
+                        System.out.println("Breaker goes up to 1000.");
+                        break;
+                    }
+                    breaker++;
+                } while (!line.startsWith("CommandTime="));
+
+                line = line.replace("CommandTime=", "");
+                if(StringUtils.isNumeric(line)){
+                    int wait = Integer.parseInt(line) + 10;
+                    Thread.sleep(wait);
+                } else {
+                    System.out.println("Error in command timing reading.");
+                    serial.writeLine("G01 X0 Y0 Z0");
+                    break;
+                }
+                executed++;
+            }
+
+        }
+        gCode.close();
+
+        return executed;
+    }
+
+
+
+    private static int executeTest(TwoWaySerialCommunication serial) throws InterruptedException {
+
+        final String[] commands = { "G00 X330 Y330", "G00 X0 Y0" };
+        final int cycleNumber = 10;
+
+        for(int i = 0; i < cycleNumber; i++){
+            final String command = commands[i%2];
+            serial.writeLine(command);
+
+            String line;
+            int breaker = 0;
+            do {
+                line = serial.readLine();
+                System.out.println(line);
+                if(breaker > 1000){
+                    System.out.println("Breacker goes up to 1000.");
+                    break;
+                }
+                breaker++;
+            } while (!line.startsWith("CommandTime="));
+
+            line = line.replace("CommandTime=", "");
+            if(StringUtils.isNumeric(line)){
+                int wait = Integer.parseInt(line) + 100;
+                Thread.sleep(wait);
+            } else {
+                System.out.println("Error in command timing reading.");
+                serial.writeLine("G01 X0 Y0 Z0");
+                break;
+            }
+        }
+
+        return cycleNumber;
     }
 
 }
